46e7e3d2cb62d902f5b95ef93bb7efcf27db8a3a
Gabriel <koymattk@gmail.com>
2021-02-23 09:54:35 -0300

    add primeira rota e adicionando conexão com o banco de dados sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
+import 'reflect-metadata';
+import './database';
 import express from 'express';
 
-const app = express();
+const app = express()
+
+app.get('/' ,(req,res)=>{
+    res.send({mesage:"Olá NLW"})
+})
 
 app.listen(3000, () => {console.log("server is running !")})
