54fe148e9238ad9d504f42e54ec91f476d9b87af
Gabriel <koymattk@gmail.com>
2021-02-23 11:19:26 -0300

    criando rota para criação de usuario

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 class UserController {
     async create(req: Request, res: Response) {
-        const user = req.body;
-        console.log(user);
-        return res.send({user});
+
+        const {name, email} = req.body;
+        const usersRepository = getRepository(User);
+        const userExists = await usersRepository.findOne({email})
+        if(userExists){
+            return res.status(400).json({error:"User already exists"})
+        }
+        const user = usersRepository.create({name, email});
+
+        await usersRepository.save(user);
+
+        return res.json(user);
+
     }
 }
 
