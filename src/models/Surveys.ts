5d9616d6fd0e73ab90018dad2043fe2bb215886b
Gabriel <koymattk@gmail.com>
2021-02-24 10:21:40 -0300

    criando model surveys

---
+++ src/models/Surveys.ts
@@ -0,0 +1,23 @@
+import { PrimaryColumn, Column, CreateDateColumn, Entity } from 'typeorm'
+import { v4 as uuid} from 'uuid'
+
+@Entity("surveys")
+class Surveys {
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if(!this.id){
+            this.id = uuid();
+        }
+    }
+}
