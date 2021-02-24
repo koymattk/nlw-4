59df5c0e07701a55f2491eb144c312051e72cbc5
Gabriel <koymattk@gmail.com>
2021-02-24 10:03:47 -0300

    Refatorando controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
 class UserController {
     async create(req: Request, res: Response) {
 
         const {name, email} = req.body;
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UserRepository);
         const userExists = await usersRepository.findOne({email})
         if(userExists){
             return res.status(400).json({error:"User already exists"})
@@ -19,4 +19,4 @@ class UserController {
     }
 }
 
-export { UserController }
+export { UserController };
